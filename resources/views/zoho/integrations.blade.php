943b3fd0087cdedcb5f32079c42b24e8b40ec89b
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-03-10 18:57:34 +0500

    Added form

--- resources/views/zoho/integrations.blade.php
+++ resources/views/zoho/integrations.blade.php
@@ -1,33 +1,39 @@
 @extends('layouts.app')
 @section('content')
-	<script>
-	window.onload = function() {
-    $("#checkbox-1").toggle();
-    $("#checkbox-2").toggle();
-	};
-    $(function() {
-      $('.activate').click(function() {
+  <script>
+  	window.onload = function() {
       $("#checkbox-1").toggle();
       $("#checkbox-2").toggle();
-    });
-  });	
+      $("#form-button").toggle();
+  	};
+    $(function() {
+      $('.activate').click(function() {
+        $("#checkbox-1").toggle();
+        $("#checkbox-2").toggle();
+        $("#form-button").toggle();
+      });
+    });	
 	</script>
     <h2 class="text-center">Email verification</h2>
     <p class="text-center">verify you email through Email Verifier API</p>
 
     <div class="container">
 	    <div class="text-center">
-      <button class="btn btn-success activate">Activate</button>
-			<button class="btn btn-danger deactivate">Deactivate</button>
-      <br/><br/>
-      <div class="container">
-        <div class="row">
-          <label class="col-md-12" id="checkbox-1">Opt-out failed emails <input type="checkbox"></label>
-        </div>
-        <div class="row">
-          <label class="col-md-12" id="checkbox-2">Add note if email fails <input id="checkbox-2" type="checkbox"></label>
-        </div>
+        <button class="btn btn-success activate">Activate</button>
+  			<button class="btn btn-danger deactivate">Deactivate</button>
+        <br/><br/>
+
+        <form action="/verify" method="POST" accept-charset="utf-8">
+          <div class="container">
+            <div class="row">
+              <label class="col-md-12" id="checkbox-1">Opt-out failed emails <input type="checkbox"></label>
+            </div>
+            <div class="row">
+              <label class="col-md-12" id="checkbox-2">Add note if email fails <input id="checkbox-2" type="checkbox"></label>
+            </div>
+            <input id="form-button" type="submit" class="btn btn-success" value="Verify">
+          </div>
+        </form>
       </div>
-	    </div>
     </div>
 @endsection
